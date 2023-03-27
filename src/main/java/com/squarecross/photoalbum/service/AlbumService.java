b8609dfacb8bc621579ac29124d17e40ef0521d9
changgeun <ckdrmsdbs123@naver.com>
2023-03-27 16:53:30 +0900

    Test(create album,select)

---
+++ src/main/java/com/squarecross/photoalbum/service/AlbumService.java
@@ -0,0 +1,26 @@
+package com.squarecross.photoalbum.service;
+
+import com.squarecross.photoalbum.domain.Album;
+import com.squarecross.photoalbum.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service //Bean으로 등록하여 loc 컨테이너에 추가
+public class AlbumService {
+    @Autowired //컨테이너에 등록된 빈을 가져와서 사용
+    private AlbumRepository albumRepository;
+
+    public Album getAlbum(Long albumId){
+        Optional<Album> res = albumRepository.findById(albumId);
+        if (res.isPresent()){
+            return res.get();
+        }else {
+            throw new EntityNotFoundException(String.format("앨범 아이디 %d로 조회되지 않았습니다", albumId));
+
+        }
+    }
+
+}
---
+++ src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
@@ -0,0 +1,31 @@
+package com.squarecross.photoalbum.service;
+
+import com.squarecross.photoalbum.domain.Album;
+import com.squarecross.photoalbum.repository.AlbumRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest //스프링 컨테이너 내에 있는 모든 빈을 DI로 가져와서 사용하게 만듬
+@Transactional // test시 db에 저장되면 좋지 않기에 주석표시
+class AlbumServiceTest {
+
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @Autowired
+    AlbumService albumService;
+
+    @Test
+    void getAlbum() {
+        Album album = new Album();
+        album.setAlbumName("테스트");
+        Album savedAlbum = albumRepository.save(album);
+
+        Album resAlbum = albumService.getAlbum(savedAlbum.getAlbumId());
+        assertEquals("테스트", resAlbum.getAlbumName());
+    }
+}
